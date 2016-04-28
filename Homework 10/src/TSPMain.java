c00b7ab00f8fb9d542bd79e3b83c15e19eeb25c2
Scott Hillier <Sfhillier@gmail.com>
2016-04-27 23:00:33 -0400

    Reads in the contents of the file
    Reads the contents of the command line file argument of the file name
    and places each line in an array list object

--- Homework 10/src/TSPMain.java
+++ Homework 10/src/TSPMain.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -12,10 +17,25 @@ public class TSPMain {
 
 	/**
 	 * @param args
+	 * Reads in file name from command line
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		// String variable to store the command line
+		// input for the filename to be searched
+		String fileName = args[0];
+		
+		// Buffered reader and FileReader objects to grab each line
+		// of the text file and an ArrayList to store each line
+		// which is contained within the line variable
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		ArrayList<String> text = new ArrayList<String>();
+		String line = br.readLine();
+		while(line != null){
+			
+			text.add(line);
+		}
+		br.close();
 	}
 
 }
